40e61275be70f7bd9e49b6c035c82bb16fa6b33c
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2021-09-15 23:36:23 +0500

    all done omly domain verification remains

--- lib/simple_email_service-stack.ts
+++ lib/simple_email_service-stack.ts
@@ -3,6 +3,13 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as actions from "@aws-cdk/aws-ses-actions";
 import * as ses from "@aws-cdk/aws-ses";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as apigw from "@aws-cdk/aws-apigateway";
+import {
+  Effect,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "@aws-cdk/aws-iam";
 
 export class SimpleEmailServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,16 +17,28 @@ export class SimpleEmailServiceStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const role = new Role(this, "snsLambdaRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+    ///Attaching ses access to policy
+    const policy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["ses:SendEmail", "ses:SendRawEmail", "logs:*"],
+      resources: ["*"],
+    });
+    //granting IAM permissions to role
+    role.addToPolicy(policy);
+
     // creating bucket to save recieved emails
 
     const bucket = new s3.Bucket(this, "SES_Bucket", {
-      bucketName: "SES_RECIEVED_EMAILS",
+      bucketName: "ses-recieved-email",
     });
 
     // Lambda call on action
 
     const actionLambda = new lambda.Function(this, "SES_Action_Lambda", {
-      code: lambda.Code.fromInline(`export.handler = (event) => {
+      code: lambda.Code.fromInline(`exports.handler = (event) => {
         console.log("EVENT ==> ", JSON.stringify(event))
       }`),
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -34,7 +53,7 @@ export class SimpleEmailServiceStack extends cdk.Stack {
     //rule for receiptEmailRuleSet
 
     ruleset.addRule("Invoke_Lambda", {
-      recipients: ["support@usmanbinnaeem.tk"],
+      recipients: ["usmaanbinnaeem@gmail.com"],
       actions: [
         new actions.Lambda({
           function: actionLambda,
@@ -45,7 +64,7 @@ export class SimpleEmailServiceStack extends cdk.Stack {
     });
 
     ruleset.addRule("SAVE_EMAILS_IN_S3_BUCKET", {
-      recipients: [""],
+      recipients: ["usmaanbinnaeem@gmail.com"],
       actions: [
         new actions.S3({
           bucket,
@@ -53,6 +72,24 @@ export class SimpleEmailServiceStack extends cdk.Stack {
         }),
       ],
       scanEnabled: true,
+    });
+
+    //  Creating send email lambda handler
+    const emailSender = new lambda.Function(this, "SendEmail", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "lambda.handler",
+      role: role,
+    });
+
+    const api = new apigw.RestApi(this, "SendEmailEndPoint");
+    api.root
+      .resourceForPath("sendmail")
+      .addMethod("POST", new apigw.LambdaIntegration(emailSender));
+
+    // logging API endpoint
+    new cdk.CfnOutput(this, "Send email endpoint", {
+      value: `${api.url}sendmail`,
     });
   }
 }
